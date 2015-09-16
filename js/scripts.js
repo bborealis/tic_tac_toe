faeb001e384c8ac8fd1e85e5925242bfa299fdab
Brian Borealis & Chris Lytsell <student@epicodus.com>
2015-09-16 11:53:36 -0700

    add board and game test functions and pass tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,36 @@
-function Player(playerName, mark){
-    this.playerName = playerName;
+function Player(mark){
     this.mark = mark;
 }
 
 function Space(x_axis, y_axis){
     this.x_axis = x_axis;
     this.y_axis = y_axis;
+}
+
+function Game(p1, p2, board){
+    this.p1 = p1;
+    this.p2 = p2;
+    this.board = board;
+}
+
+function Board(){
+    var empty = "";
+    this.spaces = [ empty, empty, empty, empty, empty, empty, empty, empty, empty ];
+}
+
+Game.prototype.win = function(board){
+    var combos = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+                    [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
+    for (var i = 0; i < combos.length; i++){
+        for(var j = 0; j < combos[i].length; j++){
+            console.log(combos[i] + " = combo index 'i': " + combos[i][j] + " = combos[i][j]");
+            if((board.spaces[combos[i][j]] === board.spaces[combos[i][j+1]]) &&
+                (board.spaces[combos[i][j+1]] === board.spaces[combos[i][j+2]])
+                && (board.spaces[combos[i][j]] !== "")){
+                return board.spaces[combos[i][j]];
+            }
+        }
+    }
 }
 
 Space.prototype.mark_by = function(player){
@@ -20,4 +45,45 @@ Space.prototype.spaceCheck = function(x, y, player){
     if (this.markedBy() === player){
         return true;
     } else {return false;}
+}
+
+Board.prototype.markedSquare = function(space){
+    var x = space.x_axis;
+    var y = space.y_axis;
+    if (x === 1) {
+        if (y === 1 ) {
+            this.spaces[0] = space.markedBy().mark;
+            return this.spaces[0];
+        } else if (y === 2){
+            this.spaces[1] = space.markedBy().mark;
+            return this.spaces[1];
+        } else {
+            this.spaces[2] = space.markedBy().mark;
+            return this.spaces[2];
+        }
+    }
+    if (x === 2) {
+        if (y === 1 ) {
+            this.spaces[3] = space.markedBy().mark;
+            return this.spaces[3];
+        } else if (y === 2){
+            this.spaces[4] = space.markedBy().mark;
+            return this.spaces[4];
+        } else {
+            this.spaces[5] = space.markedBy().mark;
+            return this.spaces[5];
+        }
+    }
+    if (x === 3) {
+        if (y === 1 ) {
+            this.spaces[6] = space.markedBy().mark;
+            return this.spaces[6];
+        } else if (y === 2){
+            this.spaces[7] = space.markedBy().mark;
+            return this.spaces[7];
+        } else {
+            this.spaces[8] = space.markedBy().mark;
+            return this.spaces[8];
+        }
+    }
 }
--- specs/specs.js
+++ specs/specs.js
@@ -1,7 +1,7 @@
 describe('Player', function() {
     it("Takes a user name and 'marker' and creates a new player", function() {
-        var testPlayer = new Player("Joe", "X");
-        expect(testPlayer.playerName, testPlayer.mark).to.be.eql("Joe", "X");
+        var testPlayer = new Player("X");
+        expect(testPlayer.mark).to.be.eql("X");
     });
 });
 
@@ -17,17 +17,56 @@ describe('Space', function() {
     });
 
     it("lets a player mark a space", function(){
-        var testPlayer = new Player("Joe", "X");
+        var testPlayer = new Player("X");
         var testSpace = new Space(1, 2);
         testSpace.mark_by(testPlayer);
         expect(testSpace.markedBy()).to.equal(testPlayer);
     });
 
     it("determines if a space is taken by a player", function(){
-        var testPlayer = new Player("Joe", "X");
+        var testPlayer = new Player("X");
         var testSpace = new Space(1, 2);
         testSpace.mark_by(testPlayer);
         var taken = true;
         expect(testSpace.spaceCheck(1, 2, testPlayer)).to.equal(taken);
+    });
+});
+
+describe('Game', function(){
+    it("Takes two players and a board and creates a game", function(){
+        var testPlayer = new Player("X");
+        var testPlayer2 = new Player("O");
+        var testBoard = new Board();
+        var testGame = new Game(testPlayer, testPlayer2, testBoard);
+        expect(testGame.p1, testGame.p2, testGame.board).to.eql(testPlayer, testPlayer2, testBoard);
+    });
+
+    it("Checks marked spaces for winning combinations", function(){
+        var testPlayer = new Player("X");
+        var testPlayer2 = new Player("O");
+        var testBoard = new Board();
+        var testGame = new Game(testPlayer, testPlayer2, testBoard);
+        var testSpace1 = new Space(2,1);
+        var testSpace2 = new Space(2,2);
+        var testSpace3 = new Space(2,3);
+        testSpace1.mark_by(testPlayer);
+        testSpace2.mark_by(testPlayer);
+        testSpace3.mark_by(testPlayer);
+        testBoard.markedSquare(testSpace1);
+        testBoard.markedSquare(testSpace2);
+        testBoard.markedSquare(testSpace3);
+        expect(testGame.win(testBoard)).to.eql(testPlayer.mark);
+    });
+});
+
+describe('Board', function(){
+    it("Takes a space and marks the position on the board", function(){
+        var testPlayer = new Player("X");
+        var testPlayer2 = new Player("O");
+        var testBoard = new Board();
+        var testGame = new Game(testPlayer, testPlayer2, testBoard);
+        var testSpace = new Space(2,2);
+        testSpace.mark_by(testPlayer);
+        expect(testBoard.markedSquare(testSpace)).to.equal(testPlayer.mark);
     });
 });
