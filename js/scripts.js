78e90675387b9bafa25177de6d7d56b9e8544242
Brian Borealis & Chris Lytsell <student@epicodus.com>
2015-09-17 12:29:29 -0700

    get x and o to appear on screen

--- js/scripts.js
+++ js/scripts.js
@@ -63,76 +63,129 @@ Board.prototype.markedSquare = function(space){
     if (x === 1) {
         if (y === 1 ) {
             this.spaces[0] = space.markedBy();
-            return this.spaces[0];
+            return 0;
         } else if (y === 2){
             this.spaces[1] = space.markedBy();
-            return this.spaces[1];
+            return 1;
         } else {
             this.spaces[2] = space.markedBy();
-            return this.spaces[2];
+            return 2;
         }
     }
     if (x === 2) {
         if (y === 1 ) {
             this.spaces[3] = space.markedBy();
-            return this.spaces[3];
+            return 3;
         } else if (y === 2){
             this.spaces[4] = space.markedBy();
-            return this.spaces[4];
+            return 4;
         } else {
             this.spaces[5] = space.markedBy();
-            return this.spaces[5];
+            return 5;
         }
     }
     if (x === 3) {
         if (y === 1 ) {
             this.spaces[6] = space.markedBy();
-            return this.spaces[6];
+            return 6;
         } else if (y === 2){
             this.spaces[7] = space.markedBy();
-            return this.spaces[7];
+            return 7;
         } else {
             this.spaces[8] = space.markedBy();
-            return this.spaces[8];
+            return 8;
         }
     }
 }
+//create game elements
+var playerX = new Player("X");
+var playerO = new Player("O");
+var board = new Board();
+var game = new Game(playerX, playerO, board, 0);
+var switchPlayer = function(player){
+  if(player === playerX) {
+    player = playerO;
+    return player;
+  }
+  else {
+    player = playerX;
+    return player;
+  }
+}
+//create play function
+var currPlayer = playerX;
+var play = function(player){
+  $(".game div").click(function(event){
+      event.preventDefault();
+      var rawSpace = $(this).attr("id");
+      switch (rawSpace){
+          case "_0":
+              var playSpace = new Space(1, 1);
+              break;
+          case "_1":
+              var playSpace = new Space(1, 2);
+              break;
+          case "_2":
+              var playSpace = new Space(1, 3);
+              break;
+          case "_3":
+              var playSpace = new Space(2, 1);
+              break;
+          case "_4":
+              var playSpace = new Space(2, 2);
+              break;
+          case "_5":
+              var playSpace = new Space(2, 3);
+              break;
+          case "_6":
+              var playSpace = new Space(3, 1);
+              break;
+          case "_7":
+              var playSpace = new Space(3, 2);
+              break;
+          case "_8":
+              var playSpace = new Space(3, 3);
+              break;
+      }
+      console.log(playSpace);
+      if (currPlayer === playerX){
+          playSpace.mark_by(playerX, board, game);
+          var id_to_mark = board.markedSquare(playSpace);
+          var divId = "#_" + id_to_mark + " span#X";
+          $(divId).show();
+          console.log("ID = " + id_to_mark + ", html id = " + divId);
+          console.log("turns: " + game.turns);
+      } else{
+          playSpace.mark_by(playerO, board, game);
+          var id_to_mark = board.markedSquare(playSpace);
+          var divId = "#_" + id_to_mark + " span#O";
+          $(divId).show();
+          console.log("ID = " + id_to_mark + ", html id = " + divId);
+          console.log("turns: " + game.turns);
+      }
+      currPlayer = switchPlayer(currPlayer);
+      console.log(currPlayer);
 
+      play(currPlayer);
 
-
-$(".game div").click(function(event){
-    event.preventDefault();
-    var rawSpace = $(this).attr("id");
-    console.log($(this).attr("id"));
-    switch (rawSpace){
-        case "0":
-            space = new Space(1, 1);
-            break;
-        case "1":
-            var space = new Space(1, 2);
-            break;
-        case "2":
-            var space = new Space(1, 3);
-            break;
-        case "3":
-            var space = new Space(2, 1);
-            break;
-        case "4":
-            var space = new Space(2, 2);
-            break;
-        case "5":
-            var space = new Space(2, 3);
-            break;
-        case "6":
-            var space = new Space(3, 1);
-            break;
-        case "7":
-            var space = new Space(3, 2);
-            break;
-        case "8":
-            var space = new Space(3, 3);
-            break;
-    }
-    console.log(space);
-    return space;
+      console.log(currPlayer);
+      //////////////////////////
+  });
+}
+// start game
+$("button").click(function(event){
+  event.preventDefault();
+  $(this).hide();
+  $("#playerX").show("slow");
 });
+
+play(currPlayer);
+
+//////////////////////////
+// player1 play()
+//    check for win
+
+// switch players
+
+// player2 play()
+//    check for win
